74b6dabab9d98a8f32b6c5e22ac7facf1076d525
Akiva Bloch <akiva.bloch@bluevine.com>
2015-02-02 13:13:56 +0200

    ng-repeat

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,11 @@
 
 	var app = angular.module("angulearn",[]);
 	app.controller('MyController', function($scope) {
-		$scope.myName="akiva";
+		$scope.people=[
+			{name:"Akiva"},
+			{name:"Justin"},
+			{name:"Uzi"},
+			{name:"Yona"},
+		]
 	})
 })()
