60f2c9d1da9313d53136bde2acc3a5b53fb6bedd
Akiva Bloch <akiva.bloch@bluevine.com>
2015-02-04 11:55:22 +0200

    support post contact

--- www/api/contacts.php
+++ www/api/contacts.php
@@ -5,4 +5,11 @@ if ($_SERVER['REQUEST_METHOD']==="GET")
 	$arr=iterator_to_array($db->contacts->find());
 	echo json_encode($arr);
 }
+elseif ($_SERVER['REQUEST_METHOD']==="POST") 
+{
+	$body=file_get_contents('php://input');
+	$arr=json_decode($body);
+	$db->contacts->insert($arr);
+	print json_encode($arr);
+}
 ?>
