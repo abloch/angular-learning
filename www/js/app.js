1d4e7a952c85ac6ac29bc63851feb0f5599f2a08
Akiva Bloch <akiva.bloch@bluevine.com>
2015-02-04 14:44:28 +0200

    one controller to rule them all

--- www/js/app.js
+++ www/js/app.js
@@ -1,42 +1,45 @@
 (function() {
 
 	var app = angular.module("phoneBook",[ ]);
-
-	app.directive("contactAdder",["$http", function(){
-		return{
-			restrict: "E",
-			templateUrl: "people-adder.html",
-			controllerAs: "adder",
-			controller: function($scope,$http){
-				this.submitter=function(){
-					$http.post("/api/contacts.php",this);
-				};
-				this.add=function(contactsCtrl){
-					contactsCtrl.push({"name":this.name,"email":this.email})
-					//window.console.log(contactsCtrl);
-					this.name="";this.email="";
-				};
-			}
+	app.controller('contactsCtrl', ['$http','$scope', function($http,$scope){
+		this.submitter=function(){
+			$http.post("/api/contacts.php",this);
 		};
-	}]);
+		this.adder=function(){
+			window.console.log(this.list);
+			this.list.push({"name":this.name,"email":this.email})
+			window.console.log(this.list);
+			this.name="";this.email="";
+		};
+		this.fetcher=function()
+		{
+			$http.get("/api/contacts.php").success(function(data){
+				window.console.log(data);
+				window.console.log($scope.contacts.list);
+				$scope.contacts.list=data;
+				window.console.log($scope.contacts.list);				
+			});			
+		};
 
-	app.directive("contacts",["$http", function($http){
+		// initializations
+		this.list=[];
+		this.limiter = 12;	
+		this.fetcher();
+		this.name="daniel";this.email="daniel@gmail.com";		
+	}])
+
+	app.directive("contactAdder",function(){
+		return{
+				restrict: "E",
+				templateUrl: "people-adder.html",
+			};
+		});	
+
+	app.directive("contacts",function($http){
 		return {
-			restrict: "E",
-			templateUrl: "people-list.html",
-			controllerAs: "contacts",			
-			controller: function($scope) {
-				$scope.list=[];
-				$scope.limiter = 7;	
-				$scope.fetcher=function()
-				{
-					$http.get("/api/contacts.php").success(function(data){
-						window.console.log(data);
-						$scope.list=data;
-					});
-				},
-				$scope.fetcher();
-			},
-		}
-	}]);
+				restrict: "E",
+				templateUrl: "people-list.html",
+			};
+		
+	});
 })()
